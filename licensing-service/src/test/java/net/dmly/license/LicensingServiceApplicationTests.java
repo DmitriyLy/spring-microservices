1b635110f7a34b28f17b279ad77b60ad439e6bfd
Dmytro Lysai <dmitriy.lisay@gmail.com>
2025-09-25 21:51:12 +0300

    ch2

---
+++ licensing-service/src/test/java/net/dmly/license/LicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dmly.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
