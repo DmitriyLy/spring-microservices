467f35f1c59ba8fbb7c8bcb1ace7d4843d4bdbc8
Dmytro Lysai <dmitriy.lisay@gmail.com>
2025-11-08 20:39:08 +0200

    ch8 - gateway with dynamic config added

---
+++ gatewayserver/src/test/java/net/dmly/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dmly.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
